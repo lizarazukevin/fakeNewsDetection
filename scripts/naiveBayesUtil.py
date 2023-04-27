508d0b982c47af3a1f81d08166f3a2b6f0979e0e
Kevin Lizarazu <sp1cybo1@vt.edu>
2023-04-27 03:40:43 -0400

    cleaned the utils script

--- scripts/naiveBayesUtil.py
+++ scripts/naiveBayesUtil.py
@@ -6,13 +6,28 @@
 import numpy as np
 
 from utils import *
+# need to put the text extraction as part of the utils
 from collections import defaultdict
 
 class NaiveBayesPredictor:
 
-    def __init__(self, train_data, train_labels, config_path="./config.json"):
+    def __init__(self, config_path="./config.json"):
         self.env_config = loadConfig(config_path)
-        self.labels = np.unique(train_labels)
-        # self.bag = defaultdict(list)
+        self.model = defaultdict()
+        # self.word_probs = defaultdict(list)
+        # self.class_probs = defaultdict(list)
+        self.raw_train
 
         print(self.env_config)
+    
+    # here we would either need to read the path to the clean data or in data extraction have it ready for us
+    def train(self, params=None,):
+
+
+        # self.labels = np.unique(train_labels)
+
+        # d, n = train_data.shape
+        # num_classes = self.labels.size
+
+        # for c in range(num_classes):
+        #     print c
--- scripts/utils.py
+++ scripts/utils.py
@@ -4,7 +4,13 @@
 # Last Modified: 04/17/23
 
 import json
+import csv
+import pandas as pd
 import numpy as np
+import string
+import chardet
+from pandas import *
+
 
 # Loads configuration contents of JSON structure 
 def loadConfig(path: str) -> dict:
@@ -15,3 +21,80 @@ def loadConfig(path: str) -> dict:
             cfg[key] = np.array(element)
             
     return cfg
+
+
+# Loads the data from a data path and returns as a DataFrame
+def loadData(file_path: str) -> DataFrame:
+
+    # Stores the contents of csv in a DataFrame
+    readdata = pd.read_csv(file_path)
+
+    # Lowercase the data
+    cleandata = readdata.apply(lambda x: x.astype(str).str.lower())
+
+    # Remove punctuation
+    translator = str.maketrans('', '', string.punctuation)
+    cleandata['title'] = cleandata['title'].str.translate(translator)
+    cleandata['text'] = cleandata['text'].str.translate(translator)
+
+    # Returns the DataFrame with text that has been lowered and rid of punctutation
+    return cleandata
+
+
+##############################################################
+# Testing Nonsense
+##############################################################
+
+# # Loads the data from a data path and returns as a dict
+# def loadData(file_path: str) -> dict:
+
+#     # dictionary structure to hold sentence id and contents
+#     # specific to this project --> {id: {'title', 'text', 'subject', 'date', 'label'}}
+#     data_dict = dict()
+
+#     with open(file_path, 'rb') as f:
+#         res = chardet.detect(f.read())
+
+#     # opens the csv file in read mode and saves entries to dict
+#     with open(file_path, newline='', encoding=res['encoding']) as f:
+
+#         # reads the file here and delimits cells based on tabs
+#         r = csv.reader(f, delimiter='\t')
+
+#         for id, line in enumerate(r):
+
+#             # first row that saves features as column headers
+#             if id == 0:
+#                 features = [x for x in line]
+#                 continue
+            
+#             # assigns contents of each row to each feature in the dictionary
+#             data_dict[id] = { feat:line[f_id] for f_id, feat in enumerate(features) }
+
+#     print(data_dict[1])
+
+#     json_obj = json.dumps(data_dict, indent=4)
+#     with open("sample_out.json", "w") as outfile:
+#         outfile.write(json_obj)
+
+#     return data_dict
+
+
+# preprocessing // Ref: https://www.youtube.com/watch?v=Otde6VGvhWM&ab_channel=KrishNaik
+# paragraph = "bruh1"
+# text = re.sub(r'\[[0-9]*\]', ' ', paragraph)
+# text = re.sub(r'\s+', ' ', text)
+# text = text.lower()
+# text = re.sub(r'\d', '', text)
+# text = re.sub (r'\s+', '', text)
+
+# sentences = nltk.sent_tokenize(text)
+# sentences = [nltk.work_tokenize(sentence) for sentence in sentences]
+
+# for i in range(len(sentences)):
+#   sentences[i] = [word for word in sentences[i] if not in stopwords.words('english')]
+
+# model = Word2Vec(sentences, min_count=1)
+# words = model.wv.vocab
+
+# similar = model.wv.most_similar('Something')
